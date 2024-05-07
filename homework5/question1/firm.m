67b2f14047b1f074b421e1939944c830b3a383c3
Yi Zhou <166239511+yi-zhou1@users.noreply.github.com>
2024-05-07 08:55:01 -0700

    Add files via upload

---
+++ homework5/question1/firm.m
@@ -0,0 +1,6 @@
+function [k_demand] = firm(param,r,l_supply)
+
+% apply firm foc and labor market clearing and derive captial demand
+k_demand = (param.alpha/(r+param.delta))^(1/(1-param.alpha)) * l_supply;
+
+end
