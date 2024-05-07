11da9828ca4207a37bc57af3eafd16eb08f6ad44
Yi Zhou <166239511+yi-zhou1@users.noreply.github.com>
2024-05-07 08:56:13 -0700

    Add files via upload

---
+++ Homework/homework5/question1/call_parameters.m
@@ -0,0 +1,5 @@
+param.e = [0.1 0.2] ;
+param.rho = 0.03 ;
+param.lambda = [0.1 0.1] ;
+param.delta = 0.05 ;
+param.alpha = 0.35 ;
---
+++ Homework/homework5/question1/excess_k_supply.m
@@ -0,0 +1,12 @@
+function [error] = excess_k_supply(param,num,grid,r)
+
+% using market clearing to write w in terms of r
+
+w = (1-param.alpha)*(param.alpha/(r+param.delta))^(param.alpha/(1-param.alpha));
+
+[c,saving,l_supply,k_supply,g] = household(param,num,grid,r,w);
+
+k_demand = firm(param,r,l_supply);
+
+error = (k_supply-k_demand)^2;
+end
